144fca69a9949b004d5ac939096126dca7284419
ucup0 <ucup0315@gmail.com>
2022-11-22 17:03:56 +0700

    try ressource

---
+++ lib/data/sumber.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+final List<String> entries = <String>[
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L'
+];
+final List<int> colorCodes = <int>[
+  600,
+  500,
+  100,
+  600,
+  500,
+  100,
+  600,
+  500,
+  100,
+  600,
+  500,
+  100
+];
+final controllerScroll = ScrollController();
+
+class Note {
+  late int? id;
+  late String title;
+  late String description;
+
+  Note({this.id, required this.title, required this.description});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'description': description,
+    };
+  }
+
+  Note.fromMap(Map<String, dynamic> map) {
+    id = map['id'];
+    title = map['title'];
+    description = map['description'];
+  }
+}
